d2938be698659fbdbf80ddc0d43e9ff5ca7e60c5
Omar <omar.salous2@gmail.com>
2024-04-15 11:07:14 +0300

    Server Setup.

---
+++ server/controllers/mainController.js
@@ -0,0 +1,15 @@
+exports.homePage = async (req, res) => {
+    const locals = {
+        title: "NodeJs Notes",
+        description: "Free NodeJs Notes App"
+    }
+    res.render('index', locals);
+}
+
+exports.about = async (req, res) => {
+    const locals = {
+        title: "About - NodeJs Notes",
+        description: "Free NodeJs Notes App"
+    }
+    res.render('about', locals);
+}
