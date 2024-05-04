6a58f2788b9d0406a652255ae8b4606e3dddfb01
Omar <omar.salous2@gmail.com>
2024-05-04 13:43:57 +0300

    Add Access Denied Page

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(session({
     saveUninitialized: true,
     store: MonogoStore.create({
         mongoUrl: process.env.MONGODB_URI
-    })
+    }),
+    cookie: { maxAge: new Date(Date.now() + 3600000) }
 }));
 
 app.use(passport.initialize());
