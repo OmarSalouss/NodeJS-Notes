af66ddee364c1e68bf4cf9b6eff58821fea85f0d
Omar <omar.salous2@gmail.com>
2024-05-05 12:22:23 +0300

    Display note by ID

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -47,3 +47,24 @@ exports.dashboard = async (req, res) => {
         console.log("Err " + error);
     }
 }
+
+exports.dashboardViewNote = async (req, res) => {
+    const note = await Note.findById({ _id: req.params.id })
+        .where({ user: req.user.id }).lean();
+    console.log(note.title)
+    if (note) {
+        res.render('dashboard/view-note', {
+            noteID: req.params.id,
+            note,
+            layout: '../views/layouts/dashboard'
+        })
+    } else {
+        res.send("Something went wrong!!!")
+    }
+
+}
+
+exports.dashboardUpdateNote = async (req, res) => {
+
+
+}
